951751135264073d97d4d8ac81b4dcea67028a2b
Dc-Valerian <valerianpedro03@gmail.com>
2026-04-16 11:08:25 +0100

    added local storage

---
+++ Classwork/script.js
@@ -0,0 +1,85 @@
+// 1. Loops (Basic)
+// Print numbers from 1 to 10 using a loop.
+
+// 4. Functions (Basic)
+// Write multiply(a, b) that returns product.
+// Get values from prompt and display result with alert.
+
+// 5. Conditionals (Time Greeting)
+// Ask user for time (0–23):
+// 0–11 → "Good morning!"
+// 12–17 → "Good afternoon!"
+// Else → "Good evening!"
+
+// prompt("Enter time (0-23):");
+// if (time >= 0 && time <= 11) {
+//     alert("Good morning!");
+// }else if (time >= 12 && time <= 17) {
+//     alert("Good afternoon!");
+// }else {
+//     alert("Good evening!");
+// }
+
+// 6. Loops (Multiples)
+// Print all multiples of 5 between 1 and 50.
+// for (let i = 5; i <= 50; i += 5) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+// 8. Arrow Functions
+// Rewrite the multiply function using an arrow function.
+
+
+
+// 9. Loop + Condition
+// Print numbers from 1 to 20:
+// If divisible by 3 → "Fizz"
+// Else → print number
+
+
+// 10. Functions (Return + Logic)
+// Create checkAge(age):
+// If age ≥ 18 → "You can vote"
+// Else → "Too young"
+
+
+// if age1 = >= 18 {
+//     console.log("you can vote")
+
+//     else if age2 = <= 18{
+//         console.log("too young")
+//     }
+// }
+
+
+// 13. DOM Manipulation
+// Create a <p> with "Learning JavaScript is fun!"
+// Use JS to:
+// Change text color to red
+// Change font size to 24px
+
+
+// 14. Simple Counter App
+// Create:
+// Increase button
+// Decrease button
+// Display count
+// Update count on click
+
+// 15. Creating & Removing Elements
+// Create:
+// Button "Add Item"
+// Button "Remove Item"
+// <ul>
+// Tasks:
+// Add <li> on click
+// Remove last <li>
+
+// 16. Form Validation (Intermediate Starter)
+// Create input for username
+// If empty:
+// Show "Field cannot be empty"
+
